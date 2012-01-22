88789182127fcbe41a6fec6803824bb23b7db96c
Jorge Rodriguez <jorge@lunarworkshop.com>
2012-01-22 14:45:49 -0800

    First draft of a new interaction mode and key config -- EASY MODE BLENDER!!! Stupid name, I guess.

---
+++ release/scripts/presets/interaction/easy_mode.py
@@ -0,0 +1,15 @@
+import bpy
+user_preferences = bpy.context.user_preferences
+
+user_preferences.view.show_tooltips_python = False
+user_preferences.view.use_rotate_around_active = True
+user_preferences.edit.use_drag_immediately = False
+user_preferences.edit.use_insertkey_xyz_to_rgb = True
+user_preferences.inputs.invert_mouse_zoom = False
+user_preferences.inputs.select_mouse = 'LEFT'
+user_preferences.inputs.use_emulate_numpad = False
+user_preferences.inputs.use_mouse_continuous = False
+user_preferences.inputs.use_mouse_emulate_3_button = False
+user_preferences.inputs.view_rotate_method = 'TURNTABLE'
+user_preferences.inputs.view_zoom_axis = 'VERTICAL'
+user_preferences.inputs.view_zoom_method = 'DOLLY'
